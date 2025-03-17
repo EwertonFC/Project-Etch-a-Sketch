1cff8c96a10c00bf3b8363c41fccca45555ffb3a
EwertonFC <99198148+EwertonFC@users.noreply.github.com>
2025-03-16 22:53:44 -0300

    descricao do commit anterior

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 let container = document.querySelector("#container")
 let quadradinhos = 16
-let tamanhoQuadradinhos = 400 / quadradinhos 
+let tamanhoQuadradinhos = 400 / quadradinhos
 
-for (let i = 0; i < quadradinhos**2; i++){
+for (let i = 0; i < quadradinhos ** 2; i++) {
     let grid = document.createElement("div")
-    grid.style.width = `${tamanhoQuadradinhos-2}px` 
-    grid.style.height = `${tamanhoQuadradinhos-2}px`
+    grid.style.width = `${tamanhoQuadradinhos - 2}px`
+    grid.style.height = `${tamanhoQuadradinhos - 2}px`
     container.appendChild(grid)
     grid.setAttribute("class", "grid")
 }
 
 let cadaquadrado = document.querySelectorAll(".grid")
 cadaquadrado.forEach(element => {
-    element.addEventListener("mouseenter", ()=> {
+    element.addEventListener("mouseenter", () => {
         element.style.backgroundColor = "black"
     })
 });
 
 
 let botaoLimpar = document.querySelector("#limpar")
-botaoLimpar.addEventListener("click", ()=>{
+botaoLimpar.addEventListener("click", () => {
     cadaquadrado.forEach(element => {
-        if(element.style.backgroundColor === "black"){
+        if (element.style.backgroundColor === "black") {
             element.style.backgroundColor = "white"
         }
 
